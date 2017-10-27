d08cc3bf80fcfb044d5ce23d0bef164512798d3b
Debasish Kalia (Wipro Ltd.) <v-debkal@microsoft.com>
2017-10-27 10:55:41 +0530

    AngularProject Basic SetUp WithoutCLI

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector:"companyname",
+  template:`<div>{{name}}</div>`
+  
+})
+
+export class TitleComponent{ 
+  
+  name:string;
+  constructor()
+  {
+     this.name="Hello Wipro !!";
+  }
+}
