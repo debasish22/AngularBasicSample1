358caa5e950a95a328480cf8ce172919410e1ae9
Debasish <kkdebasish@gmail.com>
2018-03-29 11:49:41 +0530

    Added Input as getter and setter

--- InputOutput/child.component.ts
+++ InputOutput/child.component.ts
@@ -10,11 +10,22 @@ import { Input,Output, EventEmitter } from "@angular/core";
 
 export class inOutChildComponent{
 
-    @Input ('msgForChild') inputMessage:string;
-    @Output('outPutMsgFromChild') outputMessage=new EventEmitter<number>();
+  //variables
+  private num1:string;
+  private num2:string;
+  private _message:string;
+   
+  //input/Output 
 
-    num1:string;
-    num2:string;
+  @Input ('msgForChild') inputMessage:string;
+  @Output('outPutMsgFromChild') outputMessage=new EventEmitter<number>();
+
+  //input as getter and setter
+
+  @Input() set message(message:string){
+    this._message=message;
+  }
+  get message():string{ return this._message;}
 
   addnumber(){
   this.outputMessage.emit(parseInt(this.num1) + parseInt(this.num2));
--- InputOutput/inputoutput.routing.ts
+++ InputOutput/inputoutput.routing.ts
@@ -3,6 +3,6 @@ import { inputoutputparentcomponent } from './parent.component';
 
 
 export const route:Routes=[
-    {path:'parent', component:inputoutputparentcomponent},
+    {path:'', component:inputoutputparentcomponent},
     
 ];
