32c93936a158ada0930c8fe1ce7d79d74488381f
Debasish <kkdebasish@gmail.com>
2018-03-30 14:22:36 +0530

    dataBinding HomeComponent Change

--- dataBinding/home.component.ts
+++ dataBinding/home.component.ts
@@ -3,15 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'homeComponentForDataBinding',
     templateUrl:'dataBinding/Views/home.component.html',
-    // template: `
-    //   <a [routerLink]="['/']">InterPolation</a>
-    //   <a [routerLink]="['/element']">ElementPropertyBinding</a>
-    //   <a [routerLink]="['/event']">EventBinding</a>
-    //   <a [routerLink]="['/twoway']">TwoWay Binding</a>
-    //   <div class="outer-outlet">
-    //     <router-outlet></router-outlet>
-    //   </div>
-    // `
-  })
+    })
 
 export class HomeComponent{}
